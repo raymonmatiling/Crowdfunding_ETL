d82d0fa43482131fd352c83dd9516ad7d8bc1a1d
raymonmatiling <149120878+raymonmatiling@users.noreply.github.com>
2024-01-31 21:19:38 -0500

    Add files via upload

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,57 @@
+﻿-- drop tables if exist
+
+drop table if exists contacts;
+drop table if exists category;
+drop table if exists dsubcategory;
+drop table if exists campaign;
+
+-- create contacts table and set our primary key
+
+CREATE TABLE contacts (
+    contact_id int NOT NULL,
+    first_name varchar (150)  NOT NULL,
+    last_name varchar (150)  NOT NULL,
+    email varchar (150)  NOT NULL,
+	primary key (contact_id)
+);
+
+-- create category table and set our primary key
+
+CREATE TABLE category (
+    category_id varchar (150)   NOT NULL,
+    category varchar (150)  NOT NULL,
+	primary key (category_id)
+);
+
+-- create subcategory table and set our primary key
+
+CREATE TABLE subcategory (
+    subcategory_id varchar (150)  NOT NULL,
+    subcategory varchar (150)  NOT NULL,
+	primary key (subcategory_id)
+);
+
+-- create campaign table and set our primary key and foreign keys
+
+CREATE TABLE campaign (
+    cf_id INTEGER  NOT NULL ,
+    contact_id INTEGER  NOT NULL ,
+    company_name VARCHAR(50)  NOT NULL ,
+    description TEXT  NOT NULL ,
+    goal FLOAT  NOT NULL ,
+    pledged FLOAT  NOT NULL ,
+    outcome VARCHAR(50)  NOT NULL ,
+    backers_count INTEGER  NOT NULL ,
+    country VARCHAR(50)  NOT NULL ,
+    currency VARCHAR(50)  NOT NULL ,
+    launched_date DATE  NOT NULL ,
+    end_date DATE  NOT NULL ,
+    category_id VARCHAR(50)  NOT NULL ,
+    subcategory_id VARCHAR(50)  NOT NULL ,
+	CONSTRAINT "primary_campaign" PRIMARY KEY ("cf_id")	
+);
+
+select * from contacts
+select * from category
+select * from subcategory
+select * from campaign
